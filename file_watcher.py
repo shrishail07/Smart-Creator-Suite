31c48915056db6789cd6d9ceb3a034a150fe8ea1
shrishail07 <144917838+shrishail07@users.noreply.github.com>
2026-05-09 12:37:32 +0530

    Update file_watcher.py

--- file_watcher.py
+++ file_watcher.py
@@ -1,6 +1,5 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
 import shutil
 import os
 import time
@@ -9,7 +8,6 @@ import time
 class FileHandler(FileSystemEventHandler):
 
     def __init__(self, target_folder):
-
         self.target_folder = target_folder
 
     def on_created(self, event):
@@ -17,33 +15,23 @@ class FileHandler(FileSystemEventHandler):
         if event.is_directory:
             return
 
-        file_path = event.src_path
+        file_name = os.path.basename(event.src_path)
 
-        file_name = os.path.basename(file_path)
-
-        target_path = os.path.join(
-            self.target_folder,
-            file_name
-        )
+        target_path = os.path.join(self.target_folder, file_name)
 
         time.sleep(1)
 
-        shutil.move(file_path, target_path)
+        shutil.move(event.src_path, target_path)
 
         print(f"Moved: {file_name}")
 
 
 def start_watcher(source_folder, target_folder):
 
-    event_handler = FileHandler(target_folder)
-
+    handler = FileHandler(target_folder)
     observer = Observer()
 
-    observer.schedule(
-        event_handler,
-        source_folder,
-        recursive=False
-    )
+    observer.schedule(handler, source_folder, recursive=False)
 
     observer.start()
 
