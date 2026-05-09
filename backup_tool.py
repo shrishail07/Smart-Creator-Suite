a418a96c00bcafa190b867fcce12888fddb9eb1c
shrishail07 <144917838+shrishail07@users.noreply.github.com>
2026-05-09 12:36:28 +0530

    Update backup_tool.py

--- backup_tool.py
+++ backup_tool.py
@@ -1,7 +1,6 @@
 import shutil
 import os
 import zipfile
-
 from datetime import datetime
 
 
@@ -9,10 +8,7 @@ def backup_folder(source, destination):
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    backup_path = os.path.join(
-        destination,
-        f"backup_{timestamp}"
-    )
+    backup_path = os.path.join(destination, f"backup_{timestamp}")
 
     shutil.copytree(source, backup_path)
 
@@ -23,14 +19,11 @@ def compress_folder(folder_path):
 
     zip_path = folder_path + ".zip"
 
-    with zipfile.ZipFile(zip_path, 'w') as z:
+    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as z:
 
         for root, dirs, files in os.walk(folder_path):
-
             for file in files:
-
                 file_path = os.path.join(root, file)
-
                 z.write(file_path)
 
     return zip_path
@@ -39,7 +32,6 @@ def compress_folder(folder_path):
 def restore_backup(zip_file, extract_to):
 
     with zipfile.ZipFile(zip_file, 'r') as z:
-
         z.extractall(extract_to)
 
     return "Backup Restored Successfully"
